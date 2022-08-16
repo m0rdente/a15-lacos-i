e9745c89543357c443babb1e70091e5a418a6e48
Priscila Mordente <priscilamordente@gmail.com>
2022-08-16 15:54:36 -0300

    feito exercicio a015

---
+++ exercicio3/script.js
@@ -0,0 +1,7 @@
+const maioresPaises = ["1-Rússia", "2-Canadá", "3-China", "4-EUA", "5-Brasil"]
+
+for(let i=0;i<maioresPaises.length;i++){
+
+console.log("RANK MAIORES PAISES:")
+console.log(maioresPaises[i]);
+}
